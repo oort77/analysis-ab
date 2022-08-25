b54ac8a3f37d925b349bcbcad40f8ea538d879a3
oort77 <gm@og.ly>
2022-08-25 04:29:08 +0300

    Change formatting

--- pages/1_Comp100.py
+++ pages/1_Comp100.py
@@ -289,12 +289,13 @@ st.altair_chart(ratings_histo)
 # ADR histogram
 
 adr_histo = (
-    alt.Chart(df).mark_bar(
-        size=10, color="darkorange")  # & datum.guests>10'
+    alt.Chart(df).mark_bar(size=10, color="darkorange")  # & datum.guests>10'
     .encode(
         alt.X("adr:Q", bin=True, scale=alt.Scale(zero=False)),
-        alt.Y("count()", 
-    ).properties(width=600, height=300, title="Average daily rates distribution"))
+        alt.Y("count()"),
+    ).properties(width=600,
+                 height=300,
+                 title="Average daily rates distribution"))
 
 st.altair_chart(adr_histo)
 #%%
